c464d00477c116363f79be700a9b6824ab0116fd
OG4HUNNIT <william_tau@student.asdah.school.nzT>
2021-11-02 13:41:44 +1300

    Changed the icon and made window better

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import tkinter as tk
 
+# convert_function gets the number from the user, checks to see which conversion is chosen, and then displays the output.
 def convert_function():
     temp = float(e1.get())
-
     if(var.get()==1):
         # F to C
         temp = ((temp-32)*5)/9
@@ -13,25 +13,49 @@ def convert_function():
 
     l1.configure(text="Result: {}".format(temp))
 
+# key_pressed will run when the "Enter" key is pressed and then it will run the convert function()
+def key_pressed(event):
+        if(event.char == "\r"):
+            convert_function() 
+
 root = tk.Tk()
+root.geometry('300x200') # width and height of the window
 
-root.title("Temperature Converter")
+root.title("Temperature Converter") # Title of the window
+root.iconbitmap('temperature_icon.ico') # displays icon
 
+root.grid_rowconfigure(0, weight=1)
+root.grid_rowconfigure(1, weight=1)
+root.grid_rowconfigure(2, weight=1)
+root.grid_rowconfigure(3, weight=1)
+root.grid_columnconfigure(0, weight=1)
+root.grid_columnconfigure(1, weight=1)
+
+# Entry widget for input from user
 e1 = tk.Entry(root)
 e1.grid(row=0, column=0, columnspan=2)
 
 var = tk.IntVar()
 
-r1=tk.Radiobutton(root, text="F to C", variable=var, value=1)
+# Radio button widget (F to C)
+r1=tk.Radiobutton(root, text="F to C", variable=var, value=1, bg="#eea698")
 r1.grid(row=1, column=0)
 
-r2=tk.Radiobutton(root, text="C to F", variable=var, value=2)
+# Radio button widget (C to F)
+r2=tk.Radiobutton(root, text="C to F", variable=var, value=2, bg="#eea698")
 r2.grid(row=1, column=1)
 
+# Button widget ("CONVERT")
 b1 = tk.Button(root, text="CONVERT", command=convert_function, bg="#ff0000", fg="#ffffff")
 b1.grid(row=2, column=0, columnspan=2)
 
-l1 = tk.Label(root, text="Result: -")
+# Label widget (displays the output)
+l1 = tk.Label(root, text="Result: -", bg="#eea698")
 l1.grid(row=3, column=0, columnspan=2)
 
+root.bind("<KeyPress>", key_pressed)
+
+root.configure(bg ="#eea698")
+root.minsize(width=300, height=200) #restrict minimum size of window
+root.maxsize(width=300, height=200) #restrict maximum size of window
 root.mainloop()
